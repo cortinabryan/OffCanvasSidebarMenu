57594f50746bb85ea64b3d63945462914f9c88d5
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2020-05-26 10:14:44 -0700

    more edits

--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-const navTriggerEl = document.querySelector(".hamburger")
+const navTriggerEl = document.querySelector(".hamburger");
 const navEl = document.querySelector("nav");
-const contentEl = document.querySelector(".content");
-const hamburgerBarsEl = document.getElementsByTagName("span");
-
-function toggleNav() {
-  navTriggerEl.addEventListener("click", function()  {
-    navEl.classList.toggle("open");
-    contentEl.classList.toggle("shift");
-    animateHamburgers();
-  })
-}
-
-function animateHamburgers() {
-  for (let item of hamburgerBarsEl){
-    item.classList.toggle("change");
-  }
-}
-
-
-toggleNav();
+const contentEl = document.querySelector(".content")
+const hamburgerBarsEl =  document.getElementsByTagName("span")
